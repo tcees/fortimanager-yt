0071d3e4ab1b19cf59ebb918c06d676dd7233c1a
Rafael Costa <costarafael159@gmail.com>
2019-01-10 16:22:23 -0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fortimanager_yt',
+      version='1.0',
+      description='Automatização de liberação de videos do YouTube no FortiManager',
+      author='Tribunal de Contas do Espirito Santo, STI',
+      url='https://tce.es.gov.br',
+      packages=['fortimanager_yt'],
+      include_package_data=True,
+      install_requires=['bs4', 'requests', 'jinja2'],
+      package_data={
+        '.': ['requests/*.xml']
+      },
+     )
